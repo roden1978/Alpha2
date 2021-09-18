9d18d43adb16c62ca466f1edcc7b409340dfff8f
roden1978 <rdn.homebox@gmail.com>
2021-09-18 20:58:01 +0500

    Create spawnPoints and other updates

---
+++ Assets/Scripts/GameScripts/SpawnPoint.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace GameScripts
+{
+    [ExecuteInEditMode]
+    public class SpawnPoint : MonoBehaviour
+    {
+        [SerializeField] private GameObject _prefab;
+        private float _radius;
+        private void OnDrawGizmos()
+        {
+            if (_prefab != null)
+            {
+                var spriteRenderer = _prefab.GetComponentInChildren<SpriteRenderer>();
+                _radius = spriteRenderer.bounds.size.y / 2;
+                Gizmos.color = Color.green;
+                Gizmos.DrawWireSphere(transform.position, _radius);
+            }
+            else
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawWireSphere(transform.position, Vector3.one.magnitude); 
+            }
+        }
+        public GameObject Prefab {get => _prefab; set => _prefab = value;}
+    }
+}
