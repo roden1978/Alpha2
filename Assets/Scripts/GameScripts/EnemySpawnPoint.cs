1e795c8530a5d16336af99e3d7244eb8ffe1efed
roden1978 <rdn.homebox@gmail.com>
2021-10-03 21:21:46 +0500

    Продолжение разработки системы управления точками спавна врагов

--- Assets/Scripts/GameScripts/EnemySpawnPoint.cs
+++ Assets/Scripts/GameScripts/EnemySpawnPoint.cs
@@ -5,17 +5,38 @@ namespace GameScripts
     [ExecuteInEditMode]
     public class EnemySpawnPoint : MonoBehaviour
     {
-        [SerializeField] private GameObject _prefab;
-        private float _radius;
+         public GameObject _prefab;
+         public Vector3 _position;
+         public Quaternion _rotation;
+         public string _name;
 
+        private void Start()
+        {
+            var pointTransform = transform;
+            _position = pointTransform.position;
+            _rotation = pointTransform.rotation;
+            _name = gameObject.name;
+        }
+
+        private void Update()
+        {
+            if (_position != transform.position)
+                _position = transform.position;
+            
+            if (_rotation != transform.rotation)
+                _rotation = transform.rotation;
+            
+            if (_name != null && _name != gameObject.name)
+                _name = gameObject.name;
+        }
         private void OnDrawGizmos()
         {
             if (_prefab != null)
             {
                 var prefabSpriteRenderer = _prefab.GetComponentInChildren<SpriteRenderer>();
-                _radius = prefabSpriteRenderer.bounds.size.y / 2;
+                var radius = prefabSpriteRenderer.bounds.size.y / 2;
                 Gizmos.color = Color.blue;
-                Gizmos.DrawWireSphere(transform.position, _radius);
+                Gizmos.DrawWireSphere(transform.position, radius);
                
             }
             else
@@ -24,7 +45,7 @@ namespace GameScripts
                 Gizmos.DrawWireSphere(transform.position, Vector3.one.magnitude); 
             }
         }
-        
+
         public GameObject Prefab {get => _prefab; set => _prefab = value;}
     }
 }
