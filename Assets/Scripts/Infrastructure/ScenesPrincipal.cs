5272a358478a8a2bba2300bdef3ca63acf7477dd
roden1978 <rdn.homebox@gmail.com>
2022-02-02 20:50:37 +0500

    Реализовал смерть игрока и перезагрузку сцены

--- Assets/Scripts/Infrastructure/ScenesPrincipal.cs
+++ Assets/Scripts/Infrastructure/ScenesPrincipal.cs
@@ -1,3 +1,5 @@
+using System;
+using Cinemachine;
 using Common;
 using PlayerScripts;
 using UnityEngine;
@@ -10,8 +12,33 @@ namespace Infrastructure
         [SerializeField] private Player _player;
         [SerializeField] private Fader _fader;
         private AsyncOperation _operation;
-       public AsyncOperation LoadLevel(int index)
+        private PlayerSpawnPoint _spawnPoint;
+        [SerializeField] private CinemachineVirtualCamera _camera;
+        private int _currentSceneIndex;
+
+        public Action UpdateHud;
+
+        private void Start()
         {
+            _player.Death += OnPlayerDeath;
+        }
+
+        private void OnPlayerDeath()
+        {
+            RestartCurrentLevel();
+        }
+
+        private void RestartCurrentLevel()
+        {
+            UnloadLevel(Game.GamePlayerData.CurrentScene);
+            LoadLevel(Game.GamePlayerData.CurrentScene);
+            ResetProgress();
+            UpdateHud?.Invoke();
+        }
+
+        public AsyncOperation LoadLevel(int index)
+        {
+            _currentSceneIndex = index;
             _operation = SceneManager.LoadSceneAsync(index, LoadSceneMode.Additive);
             _operation.completed += LevelStart;
             return _operation;
@@ -28,10 +55,22 @@ namespace Infrastructure
             if(operation.isDone)
             {
                 _fader.FadeOut();
+                Game.GamePlayerData.CurrentScene = _currentSceneIndex;
             }
-            PlayerSpawnPoint point = FindObjectOfType<PlayerSpawnPoint>();
-            _player.transform.position = point.transform.position;
+            _spawnPoint = FindObjectOfType<PlayerSpawnPoint>();
+            Transform playerTransform = _player.transform;
+            playerTransform.position = _spawnPoint.transform.position;
+            _camera.transform.position = playerTransform.position;
             _player.gameObject.SetActive(true);
         }
+        
+        private void ResetProgress()
+        {
+            Game.GamePlayerData.CurrentHealth = Game.PlayerData.Health;
+            Game.GamePlayerData.CurrentFruitScoresAmount = Game.PlayerData.FruitScoresAmount;
+            Game.GamePlayerData.CurrentCrystalsAmount = Game.PlayerData.CrystalsAmount;
+        }
+
+       
     }
 }
