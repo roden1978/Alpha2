54ccd51086b3b4332d46b4b438d035138a7d693e
roden1978 <rdn.homebox@gmail.com>
2022-02-28 22:58:34 +0500

    Начал реализацию врагов

---
+++ Assets/Scripts/EnemyScripts/Aggro.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace EnemyScripts
+{
+    public class Aggro : MonoBehaviour
+    {
+        [SerializeField] private TriggerObserver _triggerObserver;
+        [SerializeField] private AttackShooting attackShooting;
+
+        private void Start()
+        {
+            _triggerObserver.TriggerEnter += OnAggroTriggerEnter;
+            _triggerObserver.TriggerExit += OnAggroTriggerExit;
+            SwitchAttackOff();
+        }
+
+        private void OnAggroTriggerEnter(Collider2D obj) => 
+            SwitchAttackOn();
+
+        private void OnAggroTriggerExit(Collider2D obj) => 
+            SwitchAttackOff();
+
+        private void SwitchAttackOn() => 
+            attackShooting.enabled = true;
+
+        private void SwitchAttackOff() => 
+            attackShooting.enabled = false;
+    }
+}
---
+++ Assets/Scripts/EnemyScripts/TriggerObserver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace EnemyScripts
+{
+    [RequireComponent(typeof(Collider2D))]
+    public class TriggerObserver : MonoBehaviour
+    {
+        public event Action<Collider2D> TriggerEnter;
+        public event Action<Collider2D> TriggerExit;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            TriggerEnter?.Invoke(other);
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            TriggerExit?.Invoke(other);
+        }
+    }
+}
