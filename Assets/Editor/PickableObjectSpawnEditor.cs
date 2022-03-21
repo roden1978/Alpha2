ef190e2e3c172c0c4d9a6dcfb38daa5e27288aa5
roden1978 <rdn.homebox@gmail.com>
2022-03-21 23:04:20 +0500

    Revert "Доработал тулинг"
    This reverts commit 889e9590e30f6f7eb02242ba69479ff6153a933f.

--- Assets/Editor/PickableObjectSpawnEditor.cs
+++ Assets/Editor/PickableObjectSpawnEditor.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Data;
+using Infrastructure;
 using Infrastructure.EnemySpawners;
 using StaticData;
 using UnityEditor;
@@ -13,28 +13,17 @@ namespace Editor
         [DrawGizmo(GizmoType.Active | GizmoType.Pickable | GizmoType.NonSelected)]
         public static void RenderCustomGizmo(PickableObjectMarker pickableObjectMarker, GizmoType gizmo)
         {
-            SpriteRenderer spriteRenderer =
-                pickableObjectMarker.PickableObjectStaticData.Prefab.GetComponent<SpriteRenderer>();
-            Gizmos.color = Color.magenta;
+            SpriteRenderer spriteRenderer = pickableObjectMarker.PickableObjectStaticData.Prefab.GetComponent<SpriteRenderer>();
+            Gizmos.color = Color.red;
             Vector3 position = pickableObjectMarker.transform.position;
             Vector3 spriteSize = spriteRenderer.sprite.bounds.size;
             Gizmos.DrawWireCube(position, spriteSize);
-
-            Vector2 rectPosition = new Vector2(position.x - spriteSize.x / 2, position.y + spriteSize.y / 2);
-            Vector2 rectSize = new Vector2(spriteSize.x, spriteSize.y * -1);
-            
-            Sprite sprite = spriteRenderer.sprite;
-            Texture2D texture = sprite.texture.isReadable 
-                ? sprite.TextureFromSprite()
-                : sprite.texture;
-            Gizmos.DrawGUITexture(new Rect(rectPosition, rectSize), texture);
-
             Vector3 labelPosition = new Vector3(position.x - spriteSize.x / 2, position.y + spriteSize.y / 2 + .5f, 0);
             string text =
                 $"{Enum.GetName(typeof(PickableObjectTypeId), pickableObjectMarker.PickableObjectStaticData.PickableObjectTypeId)?.ToUpper()}";
             Handles.Label(labelPosition, text);
         }
 
-        
+       
     }
 }
