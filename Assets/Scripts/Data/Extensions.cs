df8e057e9992cda5035ea31efabef2b95486c19c
roden1978 <rdn.homebox@gmail.com>
2022-03-22 19:28:34 +0500

    Восстановление после ошибки

--- Assets/Scripts/Data/Extensions.cs
+++ Assets/Scripts/Data/Extensions.cs
@@ -18,5 +18,17 @@ namespace Data
             JsonUtility.ToJson(obj);
         public static T Deserialize<T>(this string json) => 
             JsonUtility.FromJson<T>(json);
+        public static Texture2D TextureFromSprite(this Sprite sprite)
+        {
+            Texture2D texture = new Texture2D(Mathf.FloorToInt(sprite.rect.width), Mathf.FloorToInt(sprite.rect.height));
+            Color[] pixels = sprite.texture.GetPixels(
+                Mathf.FloorToInt(sprite.rect.xMin),
+                Mathf.FloorToInt(sprite.rect.yMin),
+                Mathf.FloorToInt(sprite.rect.width),
+                Mathf.FloorToInt(sprite.rect.height));
+            texture.SetPixels(pixels);
+            texture.Apply();
+            return texture;
+        }
     }
 }
